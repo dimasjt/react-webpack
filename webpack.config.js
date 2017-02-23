a4ea02bfc3ef11c98b605085fe67e3416650d1c3
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-02-23 16:56:04 +0700

    add babel and add es6

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,18 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
   }
 };
 
