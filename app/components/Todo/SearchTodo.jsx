581cc41ef35f58621f4bfe101c8102f46b5c7720
Dimas J. Taniawan <dimazniawan@gmail.com>
2017-02-27 20:44:50 +0700

    add filter todos

--- app/components/Todo/SearchTodo.jsx
+++ app/components/Todo/SearchTodo.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 export default class SearchTodo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(event) {
+    var filter = event.target.value;
+
+    this.props.updateFilter(filter);
+  }
+
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="Search here..." />
+          <input type="text" placeholder="Search here..." onChange={this.handleFilter} />
         </form>
       </div>
     )
   }
+}
+
+SearchTodo.propTypes = {
+  todos: React.PropTypes.array,
+  updateFilter: React.PropTypes.func
 }
--- app/components/Todo/index.jsx
+++ app/components/Todo/index.jsx
@@ -6,23 +6,38 @@ import SearchTodo from './SearchTodo.jsx';
 import todos from '../../services/todos.js'
 
 export default class TodoWrapper extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      todos: todos
+      filter: ''
     }
+
+    this.updateFilter = this.updateFilter.bind(this);
   }
 
-  // componentWillMount() {
-  //
-  // }
+  updateFilter(value) {
+    this.setState({
+      filter: value
+    })
+  }
 
   render() {
+    const filterredTodos = todos.filter((todo) => {
+      let title = todo.title.toLowerCase();
+      let filterText = this.state.filter.toLowerCase();
+
+      if (title.indexOf(filterText) > -1) {
+        return true;
+      }
+
+      return false;
+    })
+
     return (
       <div>
-        <SearchTodo todos={this.state.todos} />
-        <ListTodos todos={this.state.todos} />
+        <SearchTodo updateFilter={this.updateFilter} />
+        <ListTodos todos={filterredTodos} />
       </div>
     )
   }
